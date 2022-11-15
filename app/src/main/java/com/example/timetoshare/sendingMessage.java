abd8be4e6203490602432b1690c549d24d08406d
Coyotere <81817622+Coyotere@users.noreply.github.com>
2022-11-15 13:05:58 -0500

    ^test

---
+++ app/src/main/java/com/example/timetoshare/sendingMessage.java
@@ -0,0 +1,14 @@
+package com.example.timetoshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sendingMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sending_message);
+    }
+}
