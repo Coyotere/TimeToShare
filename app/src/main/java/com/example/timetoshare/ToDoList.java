ebcd6b32a6716e7f34afc109f386b21d11435f2e
Coyotere <81817622+Coyotere@users.noreply.github.com>
2022-11-26 20:07:33 -0500

    Push item view for delete button

--- app/src/main/java/com/example/timetoshare/ToDoList.java
+++ app/src/main/java/com/example/timetoshare/ToDoList.java
@@ -5,20 +5,8 @@ public class ToDoList {
 
     All :
         - Incrémenter list de string pour les textes
-
-    MainActivity :
-
-
-    manageGroup :
-
-    createGroup :
-
     sendMessage :
         - RecyclerView : Chercher 3 photos aléatoires selon la période
         - Implémenter la notification
-        - RecycleView des photos : Si vide : indication
-
-
-
      */
 }
